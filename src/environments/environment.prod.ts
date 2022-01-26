dc791f54d2e8aff6e385817dd6b46ee7fc5d4bd2
ThaiVu54 <thai01654369440@gmail.com>
2022-01-26 20:29:31 +0700

    done

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  //API LOCAL
+    API_LOCAL:'http://localhost:8080/',
+  production: true,
+
+  //Thêm firebaseConfig vào;
+   firebaseConfig : {
+     apiKey: "AIzaSyCwwmd3sE_H7JFeWfXGocZdjqroDqKT1xo",
+     authDomain: "lover-4a315.firebaseapp.com",
+     projectId: "lover-4a315",
+     storageBucket: "lover-4a315.appspot.com",
+     messagingSenderId: "528075965864",
+     appId: "1:528075965864:web:0eb0703e86e94af685c99d",
+     measurementId: "G-6W16GD859L"
+  }
+
+};
